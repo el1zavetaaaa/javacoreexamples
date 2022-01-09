7505d11e5506b28c8f63f4c65a795a4df3922e23
lizalubenets <el1111zavetaaaaa@gmail.com>
2022-01-09 13:59:30 +0200

    java core examples work with string builder

--- src/main/java/javacoreexample/strings/basics/BasicStringExamples.java
+++ src/main/java/javacoreexample/strings/basics/BasicStringExamples.java
@@ -10,12 +10,16 @@ public class BasicStringExamples {
         String str = new String(chars);
         System.out.println("\nString example: " + str);
         System.out.println("\nUnicode symbols: " + str.charAt(2) + " - " + str.codePointAt(2));
+        System.out.println("\nString length: " + str.length());
 
         String str2 = "cat";
         System.out.println("\nCompare two strings using equals: " + str.equals(str2));
 
         String str3 = "CAT";
         System.out.println("Compare 2 strings using equals ignore case: " + str.equalsIgnoreCase(str3));
+
+
+        System.out.println("\nReplace old character to a new one: " + str2.replace("a","A"));
 
         String alex = String.valueOf(new Human("Alex"));
         System.out.println("\nMigrate objects to string value: " + alex);
@@ -43,6 +47,15 @@ public class BasicStringExamples {
         System.out.println("\nString to lower case: "
                 + toLowerUpperCase.toLowerCase(Locale.ROOT)
                 + "\nString to upper case: " + toLowerUpperCase.toUpperCase());
+
+
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println("\nCapacity of empty string builder: " + stringBuilder.capacity());
+        stringBuilder.append("Greetings");
+        System.out.println("Length of string builder: " + stringBuilder.length());
+        System.out.println("Return a substring from string builder: " + stringBuilder.substring(2,4));
+        System.out.println("Replace symbols in string builder: " + stringBuilder.replace(2,4,"AA"));
+        System.out.println("Change the order of symbols in string builder: " + stringBuilder.reverse() );
 
     }
 }
